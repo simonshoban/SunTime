240e0a06cf903960b4db07af33d208f9c5033d26
Simon Shoban <sshoban@tsologic.com>
2018-04-03 09:44:51 -0700

    Cleaned up InvalidURLException class

--- src/exceptions/InvalidURLException.java
+++ src/exceptions/InvalidURLException.java
@@ -2,14 +2,45 @@ package exceptions;
 
 import data.WebAddress;
 
+/**
+ * An exception that is thrown when the program receives an 
+ * Invalid URL to scrape information from.
+ * 
+ * @author Simon Shoban
+ * @version 1.0
+ */
 public class InvalidURLException extends Exception {
+    private WebAddress webAddress;
+    private String extension;
+    
+    /**
+     * Constructs an InvalidURLException object.
+     * 
+     * @param webAddress - The invalid WebAddress object
+     * @param extension - The URL extension of the invalid address
+     */
     public InvalidURLException(WebAddress webAddress, String extension) {
-        System.err.println("Invalid URL: " + webAddress.getDomain() + extension);
-        System.err.println();
-        System.err.println("Domain: " + webAddress.getDomain());
-        System.err.println("Country: " + webAddress.getCountry());
-        System.err.println("City: " + webAddress.getCity());
-        System.err.println("Extension: " + extension);
-        System.err.println();
+        this.webAddress = webAddress;
+        this.extension = extension;
+    }
+    
+    /**
+     * Prints error message.
+     */
+    public void printErrorMessage() {
+        errPrint("Invalid URL: " + webAddress.getDomain() + extension + "/n");
+        errPrint("Domain: " + webAddress.getDomain());
+        errPrint("Country: " + webAddress.getCountry());
+        errPrint("City: " + webAddress.getCity());
+        errPrint("Extension: " + extension + "\n");
+    }
+    
+    /**
+     * Helper function that prints messages to stderr.
+     * 
+     * @param message - The message to print
+     */
+    private void errPrint(String message) {
+        System.err.println(message);
     }
 }
